da7b039dcd61f44391688300caff0e9bf109d9e0
ARJUN <arjunsharman06@gmail.com>
2022-10-03 15:26:57 -0400

    added the reducer

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,22 @@
+import { useProductReducer } from './reducers';
+import React, { createContext, useContext } from 'react';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    categories: [],
+    currentCategory: '',
+  });
+  // use this to confirm it works!
+  console.log('state', state);
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
